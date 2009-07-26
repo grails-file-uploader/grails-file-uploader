c24eeec9d4c929c7d7eab998086b14f18f42586a
Lucas Teixeira <lucastex@gmail.com>
2009-07-26 15:31:10 -0300

    taglib test tasks

--- test/unit/FileUploaderTagLibTests.groovy
+++ test/unit/FileUploaderTagLibTests.groovy
@@ -1,15 +1,37 @@
 import grails.test.*
+import com.lucastex.grails.fileuploader.DownloadControllerTests
 
 class FileUploaderTagLibTests extends TagLibUnitTestCase {
+	
+	def fut
+	
     protected void setUp() {
         super.setUp()
+		fut = new FileUploaderTagLib()
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testPrettySizeBytes() {
+		assertEquals fut.prettysize(size: 850).toString(), "850b"
     }
+
+    void testPrettySize1KByte() {
+		assertEquals fut.prettysize(size: 1000).toString(), "1kb"
+	}
+	
+	void testPrettySize16KBytes() {
+		assertEquals fut.prettysize(size: 16000).toString(), "16kb"
+	}
+			
+	void testPrettySize18MBytes() {
+		assertEquals fut.prettysize(size: 18432000).toString(), "18mb"
+    }
+
+    void testPrettySize2_5GBytes() {
+		assertEquals fut.prettysize(size: 2621440000).toString(), "2.5gb"	
+	}
+
 }
