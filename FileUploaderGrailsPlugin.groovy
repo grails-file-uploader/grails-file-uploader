b29fe2b7ad8cb7b61390e68035db03cd71c22ea5
Lucas Teixeira <lucastex@gmail.com>
2009-07-26 15:30:09 -0300

    Descriptor is now 1.1

--- FileUploaderGrailsPlugin.groovy
+++ FileUploaderGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class FileUploaderGrailsPlugin {
     // the plugin version
-    def version = "1.0"
+    def version = "1.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2-M1 > *"
     // the other plugins this plugin depends on
