8867f19e280815ed2687ff4eb0d0de4e5f835152
Daniel Bower <daniel.d.bower@gmail.com>
2012-11-20 11:48:20 -0500

    update taglib and tests for grails 2.1 - also use modified controller

--- test/unit/FileUploaderTagLibTests.groovy
+++ test/unit/FileUploaderTagLibTests.groovy
@@ -1,37 +1,28 @@
+
 import grails.test.*
-import com.lucastex.grails.fileuploader.DownloadController
+import grails.test.mixin.TestFor
+@TestFor(FileUploaderTagLib)
+class FileUploaderTagLibTests {
 
-class FileUploaderTagLibTests extends TagLibUnitTestCase {
-	
-	def fut
-	
-    protected void setUp() {
-        super.setUp()
-		fut = new FileUploaderTagLib()
-    }
-
-    protected void tearDown() {
-        super.tearDown()
-    }
 
     void testPrettySizeBytes() {
-		assertEquals fut.prettysize(size: 850).toString(), "850b"
+		assert applyTemplate('<fileuploader:prettysize size="850" />') == '850b'
     }
 
     void testPrettySize1KByte() {
-		assertEquals fut.prettysize(size: 1000).toString(), "1kb"
+		assert applyTemplate('<fileuploader:prettysize size="1000" />') == '1kb'
 	}
 	
 	void testPrettySize16KBytes() {
-		assertEquals fut.prettysize(size: 16000).toString(), "16kb"
+		assert applyTemplate('<fileuploader:prettysize size="16000" />') == '16kb'
 	}
 			
 	void testPrettySize18MBytes() {
-		assertEquals fut.prettysize(size: 18432000).toString(), "18mb"
+		assert applyTemplate('<fileuploader:prettysize size="18432000" />') == '18mb'
     }
 
     void testPrettySize2_5GBytes() {
-		assertEquals fut.prettysize(size: 2621440000).toString(), "2.5gb"	
+		assert applyTemplate('<fileuploader:prettysize size="2621440000" />') == '2.5gb'
 	}
 
 }
