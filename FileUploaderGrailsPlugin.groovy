05dd261ed37423c35cace52ffa40a63e92d682d3
Daniel Bower <daniel.d.bower@gmail.com>
2012-11-26 11:47:09 -0500

    Version the plugin to 2.10

--- FileUploaderGrailsPlugin.groovy
+++ FileUploaderGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class FileUploaderGrailsPlugin {
     // the plugin version
-    def version = "2.0.9"
+    def version = "2.0.10"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
