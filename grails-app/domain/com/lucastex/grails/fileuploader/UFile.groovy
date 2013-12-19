e7a73fb295267d2bec41a72cbaad3631b232f94a
Daniel Bower <daniel.bower@infinum.com>
2013-12-19 15:43:49 -0500

    remove unused method

--- grails-app/domain/com/lucastex/grails/fileuploader/UFile.groovy
+++ grails-app/domain/com/lucastex/grails/fileuploader/UFile.groovy
@@ -1,6 +1,5 @@
 package com.lucastex.grails.fileuploader
 
-
 class UFile {
 
     Long size
@@ -19,20 +18,4 @@ class UFile {
         downloads()
     }
 
-    def afterDelete() {
-        try {
-            File f = new File(path)
-            if (f.exists()) {
-                if (f.delete()) {
-                    log.debug "File [${path}] deleted"
-                    def timestampFolder = path.substring(0, path.lastIndexOf("/"))
-                    new File(timestampFolder).delete()
-                } else {
-                    log.error "Could not delete file: ${f}"
-                }
-            }
-        } catch (Exception exp) {
-            log.error "Error deleting ufile: ${exp}"
-        }
-    }
 }
