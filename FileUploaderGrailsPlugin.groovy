b40a178bbd3a8b58bf538070d5f201634ee6f01e
Daniel Bower <daniel.bower@infinum.com>
2013-12-19 14:34:41 -0500

    Remove unnecessary bits and rev version

--- FileUploaderGrailsPlugin.groovy
+++ FileUploaderGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class FileUploaderGrailsPlugin {
     // the plugin version
-    def version = "2.0.11"
+    def version = "2.0.12"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
@@ -24,31 +24,4 @@ This is a heavily modified version with updates from visheshd and danieldbower"
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/file-uploader"
-
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
