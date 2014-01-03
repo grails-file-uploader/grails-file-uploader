071c2d0ea932e1f4219374b054fcdb74e73390f0
Daniel Bower <daniel.bower@infinum.com>
2014-01-03 14:11:38 -0500

    Add comments to UFile describing purposes of fields

--- grails-local-upload/grails-app/domain/org/grails/plugins/localupload/UFile.groovy
+++ grails-local-upload/grails-app/domain/org/grails/plugins/localupload/UFile.groovy
@@ -1,13 +1,48 @@
 package org.grails.plugins.localupload
 
+/**
+ * Stores metadata about the underlying file on the file system
+ *
+ */
 class UFile {
 
+	/**
+	 * Size of the file in bytes
+	 */
     Long sizeInBytes
+	
+	/**
+	 * Full file system path to the file
+	 */
     String path
+	
+	/**
+	 * Filename and extension
+	 */
     String name
+	
+	/**
+	 * File extension which suggests the Mime Type
+	 */
     String extension
+	
+	/**
+	 * Date the file was uploaded to the file system
+	 */
     Date dateUploaded
+	
+	/**
+	 * Number of times the file has been pulled from the file system
+	 */
     Integer downloads
+	
+	/**
+	 * Full URL to the thumbnail of this file
+	 * 
+	 * Currently this is not created by the LocalUpload Service, but you could create
+	 * your own service to search for null values in this column, and then create the 
+	 * thumbnail as a batch process.
+	 */
 	String pathToThumbnail
 	
     static constraints = {
