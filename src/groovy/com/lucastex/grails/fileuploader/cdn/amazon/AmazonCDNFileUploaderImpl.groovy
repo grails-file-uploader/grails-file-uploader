3ca97600ed5a5e1710931ef1d509cb54573aea70
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-01-09 12:33:52 +0530

    Added option to make object public. #9

--- src/groovy/com/lucastex/grails/fileuploader/cdn/amazon/AmazonCDNFileUploaderImpl.groovy
+++ src/groovy/com/lucastex/grails/fileuploader/cdn/amazon/AmazonCDNFileUploaderImpl.groovy
@@ -1,12 +1,22 @@
 package com.lucastex.grails.fileuploader.cdn.amazon
 
 import org.jclouds.ContextBuilder
+import org.jclouds.aws.s3.AWSS3Client
+import org.jclouds.aws.s3.blobstore.options.AWSS3PutObjectOptions
 import org.jclouds.blobstore.BlobStoreContext
 import org.jclouds.blobstore.domain.Blob
+import org.jclouds.s3.domain.AccessControlList
+import org.jclouds.s3.domain.CannedAccessPolicy
+import org.jclouds.s3.domain.S3Object
+import org.jclouds.s3.domain.AccessControlList.Permission
+import org.jclouds.s3.domain.internal.MutableObjectMetadataImpl
+import org.jclouds.s3.domain.internal.S3ObjectImpl
 
-import com.lucastex.grails.fileuploader.cdn.CDNFileUploader;
+import com.lucastex.grails.fileuploader.cdn.CDNFileUploader
 
 class AmazonCDNFileUploaderImpl extends CDNFileUploader {
+
+    AWSS3Client client
 
     AmazonCDNFileUploaderImpl(String accessKey, String accessSecret) {
         this.accessKey = accessKey
@@ -23,11 +33,19 @@ class AmazonCDNFileUploaderImpl extends CDNFileUploader {
         blobStore = context.getBlobStore()
         println "Blobstore ${blobStore.class}"
 
+        client = context.unwrap().getApi()
+
         return false
     }
 
     @Override
     void close() {
+        context.close()
+    }
+
+    @Override
+    boolean containerExists(String name) {
+        client.bucketExists(name)
     }
 
     @Override
@@ -36,12 +54,47 @@ class AmazonCDNFileUploaderImpl extends CDNFileUploader {
     }
 
     @Override
-    boolean uploadFile(String containerName, File file, String fileName) {
-        Blob newFileToUpload = blobStore.blobBuilder(fileName)
-                .payload(file)
-                .build()
+    void deleteFile(String containerName, String fileName) {
+        blobStore.removeBlob(containerName, fileName)
+    }
 
-        String eTag = blobStore.putBlob(containerName, newFileToUpload)
+    S3ObjectImpl getObject(String containerName, String fileName) {
+        client.getObject(containerName, fileName, null)
+    }
+
+    String getURI(String containerName, String fileName) {
+        getObject(containerName, fileName).metadata.uri
+    }
+
+    @Override
+    boolean makeFilePublic(String containerName, String fileName) {
+        AccessControlList acl = new AccessControlList()
+        acl.addPermission(new URI("http://acs.amazonaws.com/groups/global/AllUsers"), Permission.READ)
+        getObject(containerName, fileName).setAccessControlList(acl)
+    }
+
+    @Override
+    boolean uploadFile(String containerName, File file, String fileName, boolean makePublic = false) {
+        String eTag
+
+        if(makePublic) {
+            AWSS3PutObjectOptions fileOptions = new AWSS3PutObjectOptions()
+            fileOptions.withAcl(CannedAccessPolicy.PUBLIC_READ)
+
+            MutableObjectMetadataImpl mutableObjectMetadata = new MutableObjectMetadataImpl()
+            mutableObjectMetadata.setKey(fileName)
+
+            S3Object newFileToUpload = new S3ObjectImpl(mutableObjectMetadata)
+            newFileToUpload.setPayload(file)
+
+            client.putObject(containerName, newFileToUpload, fileOptions)
+        } else {
+            Blob newFileToUpload = blobStore.blobBuilder(fileName)
+                    .payload(file)
+                    .build()
+            eTag = blobStore.putBlob(containerName, newFileToUpload)
+        }
+
         return true
     }
 }
--- src/java/com/lucastex/grails/fileuploader/cdn/CDNFileUploader.java
+++ src/java/com/lucastex/grails/fileuploader/cdn/CDNFileUploader.java
@@ -4,20 +4,31 @@ import java.io.File;
 
 import org.jclouds.blobstore.BlobStore;
 import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.cloudfiles.CloudFilesClient;
 
 public abstract class CDNFileUploader {
 
     public String accessKey;
+
     public String accessSecret;
 
     public BlobStore blobStore;
+
+    public CloudFilesClient cloudFilesClient;
+
     public BlobStoreContext context;
 
     public abstract boolean authenticate();
 
     public abstract void close();
 
+    public abstract boolean containerExists(String name);
+
     public abstract boolean createContainer(String name);
+
+    public abstract void deleteFile(String containerName, String fileName);
+
+    public abstract boolean makeFilePublic(String containerName, String fileName);
 
     public abstract boolean uploadFile(String containerName, File file, String fileName);
 
