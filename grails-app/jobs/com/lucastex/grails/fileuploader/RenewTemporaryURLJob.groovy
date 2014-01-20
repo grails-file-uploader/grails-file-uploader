1605d8d21e6a555971750190dc76484ee6c93efe
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-01-20 17:45:48 +0530

    Removed copyright statement.

--- grails-app/jobs/com/lucastex/grails/fileuploader/RenewTemporaryURLJob.groovy
+++ grails-app/jobs/com/lucastex/grails/fileuploader/RenewTemporaryURLJob.groovy
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2011, CauseCode Technologies Pvt Ltd, India.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or
- * without modification, are not permitted.
- */
-
 package com.lucastex.grails.fileuploader
 
 import grails.util.Environment
