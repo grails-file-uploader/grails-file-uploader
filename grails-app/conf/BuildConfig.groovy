e08f4a86dc0ec758e57b84d69b9cb8d7b0b388d7
Shashank Agrawal <shashank.agrawal@causecode.com>
2014-05-06 11:22:00 +0530

    Upgraded plugin version. Unable to deploy to remote server.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir  = "target/test-reports"
-
+grails.project.work.dir = "target/work"
 grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
 
@@ -14,14 +14,14 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
-        compile ("org.apache.jclouds.provider:cloudfiles-us:1.7.0", "org.apache.jclouds:jclouds-compute:1.7.0",
-                "org.apache.jclouds.provider:aws-s3:1.7.0") {
+        compile ("org.apache.jclouds.provider:cloudfiles-us:1.7.2", "org.apache.jclouds:jclouds-compute:1.7.2",
+                "org.apache.jclouds.provider:aws-s3:1.7.2") {
                     excludes "jclouds-core"
                 }
         compile "org.apache.jclouds:jclouds-core:1.7.2"
     }
     plugins {
-        // Make sure to comment while packaging to allow mongo db to install & hibernate to uninstall in parent app.
+        // Make sure to comment while packaging to allow Grails Mongodb plugin to install & hibernate to uninstall in parent app.
         /*runtime (":hibernate:3.6.10.7") {
             export = false
         }*/
