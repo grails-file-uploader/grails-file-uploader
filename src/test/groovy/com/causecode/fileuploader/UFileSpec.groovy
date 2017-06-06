2b7111dbecad63bc9de177844c62524f8d0d8736
Shivam Pandey <shivam.pandey@causecode.com>
2017-06-06 13:33:30 +0530

    Added dependecies and unit test #8

--- src/test/groovy/com/causecode/fileuploader/UFileSpec.groovy
+++ src/test/groovy/com/causecode/fileuploader/UFileSpec.groovy
@@ -7,6 +7,7 @@
  */
 package com.causecode.fileuploader
 
+import com.causecode.fileuploader.embedded.EmUFile
 import grails.buildtestdata.mixin.Build
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
@@ -103,5 +104,23 @@ class UFileSpec extends Specification implements BaseTestSetup {
 
         then: 'Method returns containerName'
         result == 'test'
+    }
+
+    void "test getEmbeddedInstance method for valid fields in EmUFile instance"() {
+        given: 'An instance of UFile'
+        UFile uFileInstance = UFile.build(expiresOn: new Date(), extension: 'jpg', name: 'test file', downloads: 1)
+
+        assert uFileInstance.embeddedInstance.validate()
+
+        when: 'getEmbeddedInstance method is called'
+        EmUFile emUFile = uFileInstance.embeddedInstance
+
+        then: 'Fields of embedded instance should exist and match with created UFile'
+        emUFile.instanceId == uFileInstance.id
+        emUFile.name == uFileInstance.name
+        emUFile.expiresOn == uFileInstance.expiresOn
+        emUFile.downloads == uFileInstance.downloads
+        emUFile.path == uFileInstance.path
+        emUFile.extension == uFileInstance.extension
     }
 }
